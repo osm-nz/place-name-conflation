5a6be941a5b569fc16672bf904bd87314505b046
Kyle Hensel <k.y.l.e@outlook.co.nz>
2026-04-26 23:36:34 +1000

    add `User-Agent` to overpass api request

--- src/api/osm.ts
+++ src/api/osm.ts
@@ -1,5 +1,6 @@
 import { promises as fs } from 'node:fs';
 import path from 'node:path';
+import taginfo from '../../taginfo.template.json' with { type: 'json' };
 import { osmRawPath } from '../core/constants.js';
 import type { RawOsm } from '../core/types/osm.def.js';
 
@@ -19,6 +20,7 @@ export async function fetchOsm() {
 
   const request = await fetch(
     `https://overpass-api.de/api/interpreter?data=${encodeURIComponent(query)}`,
+    { headers: { 'User-Agent': taginfo.project.project_url } },
   );
   const apiResponse = (await request.json()) as RawOsm;
 
